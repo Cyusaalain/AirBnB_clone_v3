f8dac818e4cc23f758f880d5d1f68868fb4fb71b
Cyusaalain <138726898+Cyusaalain@users.noreply.github.com>
2024-03-31 14:22:55 +0200

    Delete api directory

--- api/v1/views/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Other imports and route definitions go here
-# ...
-
-# Make sure there are no other statements between imports and route definitions
